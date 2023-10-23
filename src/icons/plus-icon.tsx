bb59a29be290588dc38fe092a2c4685e9e7db985
Vladimir Angelov <vlady.angelov@gmail.com>
2023-10-24 00:10:17 +0300

    Fix icon props

--- src/icons/plus-icon.tsx
+++ src/icons/plus-icon.tsx
@@ -14,9 +14,9 @@ export function PlusIcon({ size, style }: Props) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="feather feather-plus"
     >
       <line x1="12" y1="5" x2="12" y2="19"></line>
